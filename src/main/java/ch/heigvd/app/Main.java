6d82162528bd39d3895c998fa9c329c2d503c60f
Alen Bijelic <alen.bijelic@heig-vd.ch>
2022-03-07 23:00:33 +0100

    Create picocli commands without their implementations

--- src/main/java/ch/heigvd/app/Main.java
+++ src/main/java/ch/heigvd/app/Main.java
@@ -1,14 +1,31 @@
 package ch.heigvd.app;
 
+import picocli.CommandLine.Command;
 
 /**
  * Hello world!
  *
  */
+@Command(name = "main")
 public class Main
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    @Command(name = "new")
+    public void newCommand(){
+        // New command to be implemented here
+    }
+
+    @Command(name = "clean")
+    public void cleanCommand(){
+        // Clean command to be implemented here
+    }
+
+    @Command(name = "build")
+    public void buildCommand(){
+        // Build command to be implemented here
+    }
+
+    @Command(name = "serve")
+    public void serveCommand(){
+        // Serve command to be implemented here
     }
 }

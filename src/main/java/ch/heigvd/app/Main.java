bc65252104c98e787b58224428be711d2a0bfb09
Anthony Coke <anthony.coke@heig-vd.ch>
2022-03-30 18:25:23 +0200

    Update Main class with version option

--- src/main/java/ch/heigvd/app/Main.java
+++ src/main/java/ch/heigvd/app/Main.java
@@ -3,19 +3,28 @@ package ch.heigvd.app;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-
 import java.util.concurrent.Callable;
+
 
 @Command(name = "static",
         mixinStandardHelpOptions = true,
-        version = "static 0.1",
         description = "Generate random static websites",
         subcommands = {New.class, Clean.class, Build.class, Serve.class})
 public class Main implements Callable<Integer>
 {
+    @CommandLine.Option(names = {"-V", "--version"}, description = "display version info")
+    boolean versionInfoRequested;
+
     @Override
-    public Integer call() throws Exception {
-        System.out.println("Static");
+    public Integer call() {
+        if (versionInfoRequested) {
+            String version = System.getProperty(
+                    "project.version");
+
+            System.out.println("Current version : " + version);
+        } else {
+            System.out.println("Static");
+        }
         return 0;
     }
 

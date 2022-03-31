d3227c0f62b510007e681111b81f0f0f8e92d7dc
Anthony Coke <anthony.coke@heig-vd.ch>
2022-03-31 13:35:28 +0200

    Rename converter test class and refactor inside

--- src/test/java/ch/heigvd/app/utils/MarkdownConverterTest.java
+++ src/test/java/ch/heigvd/app/utils/MarkdownConverterTest.java
@@ -4,46 +4,50 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class MarkdownParserTest {
+/**
+ * MarkdownConverterTest class
+ */
+public class MarkdownConverterTest {
 
    @Test
    public void parserShouldParseImagesCorrectly() {
-      String result = "<img src=\"./image.png\" alt=\"Une image\"/>";
+      String result = "<p><img src=\"./image.png\" alt=\"Une image\" /></p>\n";
       String input = "![Une image](./image.png)";
-      String output = MarkdownParser.parse(input);
+      String output = MarkdownConverter.convert(input);
 
       assertEquals(result,output);
    }
 
    @Test
    public void parserShouldParseLinksCorrectly() {
-      String result = "<a href=\"www.google.com\">Lien vers google</a>";
+      String result = "<p><a href=\"www.google.com\">Lien vers " +
+              "google</a></p>\n";
       String input = "[Lien vers google](www.google.com)";
-      String output = MarkdownParser.parse(input);
+      String output = MarkdownConverter.convert(input);
 
       assertEquals(result,output);
    }
 
    @Test
    public void parserShouldParseHeadingsCorrectly() {
-      String result1 = "<h1>Mon premier article</h1>";
+      String result1 = "<h1>Mon premier article</h1>\n";
       String input1 = "# Mon premier article";
-      String output1 = MarkdownParser.parse(input1);
+      String output1 = MarkdownConverter.convert(input1);
 
       assertEquals(result1,output1);
 
-      String result2 = "<h2>Mon sous-titre</h2>";
+      String result2 = "<h2>Mon sous-titre</h2>\n";
       String input2 = "## Mon sous-titre";
-      String output2 = MarkdownParser.parse(input2);
+      String output2 = MarkdownConverter.convert(input2);
 
       assertEquals(result2,output2);
    }
 
    @Test
    public void parserShouldParseParagraphsCorrectly() {
-      String result = "<p>Le contenu de mon article.</p>";
+      String result = "<p>Le contenu de mon article.</p>\n";
       String input = "Le contenu de mon article.";
-      String output = MarkdownParser.parse(input);
+      String output = MarkdownConverter.convert(input);
 
       assertEquals(result,output);
    }
@@ -54,16 +58,15 @@ public class MarkdownParserTest {
 
       String result = "<h1>Mon premier article</h1>\n" +
               "<h2>Mon sous-titre</h2>\n" +
-              "<p>Le contenu de mon article.</p>\n" +
-              "<img src=\"./image.png\" alt=\"Une image\"/>";
+              "<p>Le contenu de mon article.\n" +
+              "<img src=\"./image.png\" alt=\"Une image\" /></p>\n";
 
       String input = "# Mon premier article\n" +
               "## Mon sous-titre\n" +
               "Le contenu de mon article.\n" +
               "![Une image](./image.png)";
 
-
-      String output = MarkdownParser.parse(input);
+      String output = MarkdownConverter.convert(input);
 
       assertEquals(result, output);
    }

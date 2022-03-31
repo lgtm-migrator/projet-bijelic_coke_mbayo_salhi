cb5056631e9a64ed9fb87633909b5cf1e41bae91
Anthony Coke <anthony.coke@heig-vd.ch>
2022-03-31 13:28:52 +0200

    Implement convertion and add comments

--- src/main/java/ch/heigvd/app/utils/MarkdownParser.java
+++ src/main/java/ch/heigvd/app/utils/MarkdownParser.java
@@ -1,16 +1,29 @@
 package ch.heigvd.app.utils;
 
+import com.vladsch.flexmark.util.ast.Node;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.data.MutableDataSet;
 /**
- *
+ * MarkdownParser class
  */
 public class MarkdownParser {
 
    /**
-    *
-    * @param input
-    * @return
+    * Converts a markdown file to html
+    * @param input input markdown text
+    * @return input text converted to html
+    * @see <a href="https://github.com/vsch/flexmark-java">FlexMark</a>
     */
    public static String parse(String input) {
-      return "";
+
+      // Used to add options
+      MutableDataSet options = new MutableDataSet();
+
+      Parser parser = Parser.builder(options).build();
+      HtmlRenderer renderer = HtmlRenderer.builder(options).build();
+      Node document = parser.parse(input);
+
+      return renderer.render(document);
    }
 }

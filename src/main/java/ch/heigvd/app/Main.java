a6b1755c13e11ac1f020faebd1c7698e89ce810f
guilain <gmbayo11@gmail.com>
2022-04-03 11:04:46 +0200

    Add implementation of version command

--- src/main/java/ch/heigvd/app/Main.java
+++ src/main/java/ch/heigvd/app/Main.java
@@ -5,15 +5,21 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 import java.util.concurrent.Callable;
+import picocli.CommandLine.IVersionProvider;
 
 @Command(name = "static",
         mixinStandardHelpOptions = true,
-        version = "static 0.1",
         description = "Generate random static websites",
         subcommands = {New.class, Clean.class, Build.class, Serve.class,
-                Init.class})
+                Init.class},
+        versionProvider = Main.ManifestVersionProvider.class)
 public class Main implements Callable<Integer>
 {
+    @CommandLine.Option(names = {"-V", "--version"}, versionHelp = true,
+            description = "display version info")
+    boolean versionRequested;
+
+
     @Override
     public Integer call() throws Exception {
         System.out.println("Static");
@@ -24,5 +30,17 @@ public class Main implements Callable<Integer>
     {
         int exitCode = new CommandLine(new Main()).execute(args);
         System.exit(exitCode);
+    }
+
+    /**
+     * {@link IVersionProvider} implementation that returns version
+     * information from the project-bijelic_coke_mbayo_sahli jar file's {@code
+     * /META-INF/MANIFEST.MF} file.
+     * @see <a href="https://picocli.info/#_dynamic_version_information">...</a>
+     */
+    static class ManifestVersionProvider implements IVersionProvider {
+        public String[] getVersion() throws Exception {
+            return new String[]{"Current version : " + Main.class.getPackage().getImplementationVersion()};
+        }
     }
 }

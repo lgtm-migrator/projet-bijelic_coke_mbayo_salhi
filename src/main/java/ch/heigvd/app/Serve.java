532b688a867580a575c5cd14c9df1564554f753c
guilain <gmbayo11@gmail.com>
2022-04-27 13:16:21 +0200

    first commit after rebase

--- src/main/java/ch/heigvd/app/Serve.java
+++ src/main/java/ch/heigvd/app/Serve.java
@@ -4,6 +4,8 @@ import picocli.CommandLine.Command;
 
 import java.util.concurrent.Callable;
 
+import io.javalin.Javalin;
+
 @Command(name = "serve")
 public class Serve implements Callable<Integer> {
     @Override

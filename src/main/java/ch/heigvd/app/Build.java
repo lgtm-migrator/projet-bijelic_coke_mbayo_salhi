d830b5365fe625545dfed3ddc9f94769aebd56e1
Alen Bijelic <alen.bijelic@heig-vd.ch>
2022-04-27 15:39:51 +0200

    Copy directory and files in build folder

--- src/main/java/ch/heigvd/app/Build.java
+++ src/main/java/ch/heigvd/app/Build.java
@@ -1,21 +1,27 @@
 package ch.heigvd.app;
 
+import ch.heigvd.app.utils.MarkdownConverter;
+import com.ibm.icu.impl.ICULocaleService;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Set;
 import java.util.concurrent.Callable;
 
 @Command(name = "build")
 public class Build implements Callable<Integer> {
     @CommandLine.Parameters(index = "0", description = "Path to build " + "directory")
     private String sourcePath;
+
     final private String BUILD_DIRECTORY_NAME = "build";
+    final private String MARKDOWN_FILE_TYPE = "md";
+    final private Set<String> FILE_TYPE_TO_EXCLUDE = Set.of("yaml");
 
     @Override
     public Integer call() throws Exception {
@@ -23,30 +29,60 @@ public class Build implements Callable<Integer> {
 
         System.out.println("Building in : " + sourcePath);
 
-        createBuildFolder(sourcePath);
+        copyFiles(sourcePath, sourcePath + "/" + BUILD_DIRECTORY_NAME);
 
         return 0;
     }
 
     /**
-     * Create a /build folder in specified directory
+     * Recursive directory copiing with parsing for certain files
+     * @param sourcePath Directory where files are located
+     * @param buildPath Directory where files are being copied to
      */
-    private void createBuildFolder(String directory) throws IOException {
-        Path folderPath = Paths.get(directory);
-        if(Files.exists(folderPath)){
-            Path buildPath = Paths.get(folderPath + "/build");
-            Files.createDirectories(buildPath);
-        }
-        else {
-            throw new FileNotFoundException("Specified file does not exists !");
-        }
-    }
+    private void copyFiles(String sourcePath, String buildPath, CopyOption... options) throws IOException {
+        Path source = Paths.get(sourcePath);
+        Path destination = Paths.get(buildPath);
 
-    /**
-     * Recursive directory coping with parsing for certain files
-     * @param directory Directory where files are located
-     */
-    private void copyFiles(String directory){
+        if(Files.exists(destination))
+            FileUtils.deleteDirectory(destination.toFile());
+
+        Files.walkFileTree(source, new SimpleFileVisitor<>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                Files.createDirectories(destination.resolve(source.relativize(dir)));
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                String fileExtension = FilenameUtils.getExtension(file.toString());
+                if(!FILE_TYPE_TO_EXCLUDE.contains(fileExtension)){
+                    if(fileExtension.equals(MARKDOWN_FILE_TYPE)){
+                        StringBuilder HTMLContent = new StringBuilder();
+
+                        try (FileInputStream fis = new FileInputStream(file.toString());
+                             InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
+                             BufferedReader reader = new BufferedReader(isr)
+                        ) {
+                            String str;
+                            boolean startToCopy = false;
+                            while ((str = reader.readLine()) != null) {
+                                if(str.equals("==="))
+                                    startToCopy = true;
+                                if(startToCopy){
+                                    HTMLContent.append(MarkdownConverter.convert(str));
+                                }
+                            }
+                        } catch (IOException e) {
+                            System.err.println("Error while ");
+                        }
+
+                    }
+                    Files.copy(file, destination.resolve(source.relativize(file)), options);
+                }
+                return FileVisitResult.CONTINUE;
+            }
+        });
         // For each file in a directory
             // If it is a .md file
                 // parseMarkdownToHTML

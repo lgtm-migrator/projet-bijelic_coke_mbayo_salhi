4857b7f298bb2c5a5e74d79f83d07f0a4506780a
Meh <mehdi.salhi@heig-vd.ch>
2022-05-20 05:36:18 +0200

    Add jGit integration to the publish command

--- src/main/java/ch/heigvd/app/commands/Publish.java
+++ src/main/java/ch/heigvd/app/commands/Publish.java
@@ -24,7 +24,72 @@ public class Publish implements Callable<Integer> {
     @Override
     public Integer call() throws Exception {
 
-        
+        LOGGER.setLevel(Level.INFO);
+        System.out.println("publishing config/ directory on GitHub");
+
+        File localPath = new File(System.getProperty("user" + ".dir"));
+
+        System.out.println(localPath);
+
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+
+        // Check if the current directory is a git repository. If not it
+        // initialize it.
+        Git git = null;
+        try {
+            git = Git.open(localPath);
+            LOGGER.info("Already a Git repository: " + localPath);;
+        } catch (RepositoryNotFoundException e) {
+            LOGGER.info("Initialising " + localPath + " as a git repository");
+            try {
+                git = Git.init().setDirectory(localPath).call();
+                LOGGER.log(Level.INFO, "Sucessfully initialized git " +
+                        "repository");
+
+                System.out.println("Enter the repository url: ");
+                String url = scanner.nextLine();  // Read user input
+                System.out.println("url: " + url);  // Output user input
+
+                // add remote repository
+                RemoteAddCommand remoteAddCommand = git.remoteAdd();
+                remoteAddCommand.setName("origin");
+
+                remoteAddCommand.setUri(new URIish(url));
+
+                remoteAddCommand.call();
+                LOGGER.log(Level.INFO, "Sucessfully set remote");
+            } catch (GitAPIException e1) {
+                e1.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Ask the user to enter the remote git repository url and their
+        // GitHub Access token
+
+        System.out.println("Enter your GitHub access token: ");
+
+        String token = scanner.nextLine();  // Read user input
+
+        // test values with a test account on github
+        //String token = "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";
+        //String token2 = "ghp_xr7bpujomx3bvqpuyfdr04pywfdfdb2unsx2";
+        //String httpUrl = "https://github.com/diltest/DIL-TEST.git";
+
+        git.add().addFilepattern("./build/").call();
+        LOGGER.log(Level.INFO, "Added build/ directory to stagging");
+
+        git.commit().setMessage("static publish build directory").setSign(false).call();
+
+        // push to remote
+        PushCommand pushCommand = git.push();
+
+        pushCommand.setCredentialsProvider(new
+                UsernamePasswordCredentialsProvider(token, ""));
+        pushCommand.call();
+
+        LOGGER.log(Level.INFO, "Push was successful");
 
         return 0;
     }
